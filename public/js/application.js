300d235503040c00f9aa6900c7798f67471ee6f8
Jack McCallum <mccallumjack@gmail.com>
2015-06-25 14:43:42 -0500

    added in materialize template

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
 });
+
+
+
+// From Materialize Init File
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
