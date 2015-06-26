94b15552aefd5bd6d99a4e3ef56fe00643d80246
Jack McCallum <mccallumjack@gmail.com>
2015-06-26 09:10:51 -0500

    ran migration

--- db/migrate/20150626090001_create_contribution_summaries.rb
+++ db/migrate/20150626090001_create_contribution_summaries.rb
@@ -2,7 +2,7 @@ class CreateContributionSummaries < ActiveRecord::Migration
   def change
     create_table :contribution_summaries do |t|
       t.integer  :politician_id
-      t.string   :state, ,:cid, :party, :chamber, :party, :total, :spend, :cash_on_hand, :debt, :origin
+      t.string   :state ,:cid, :party, :chamber, :party, :total, :spend, :cash_on_hand, :debt, :origin
       t.timestamps
     end
   end
